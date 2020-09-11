91059d0dc011165f70798249bb190a41049eba86
Sudam Bandara <sudam@sudam.co.nz>
2020-09-12 09:40:42 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,18 +11,13 @@ echo $START_MESSAGE
 echo
 echo "Downloading datasets..."
 wget -q -O tmp.zip https://zenodo.org/record/4007570/files/twitter-1.0.0.zip && unzip tmp.zip && rm tmp.zip
-mv twitter-1.0.0.xml src/resources/
+mv twitter-1.0.0/twitter-1.0.0.xml src/resources/
 wget -q -O tmp.zip https://zenodo.org/record/4007570/files/googletasks-1.0.0.zip && unzip tmp.zip && rm tmp.zip
-mv googletasks-1.0.0.xml src/resources/
+mv googletasks-1.0.0/googletasks-1.0.0.xml src/resources/
 wget -q -O tmp.zip https://zenodo.org/record/4007570/files/slack-1.0.0.zip && unzip tmp.zip && rm tmp.zip
-mv slack-1.0.0.xml src/resources/
+mv slack-1.0.0/slack-1.0.0.xml src/resources/
 
-
-# mvn clean install
-# mvn -q clean compile exec:java -Dexec.mainClass="nz.ac.massey.httpmockskeletons.scripts.datapreparator.TrainingDataGenerator" -Dexec.args="-$*"
 echo
 echo $END_MESSAGE
-
-fi
 
 exit 0
